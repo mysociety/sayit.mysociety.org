4208ad676acb595c756a67444b440e99b06b2d58
Matthew Somerville <matthew-github@dracos.co.uk>
2014-10-09 10:04:27 +0100

    Only create a virtualenv if not already there.

--- conf/post_deploy_actions.bash
+++ conf/post_deploy_actions.bash
@@ -8,8 +8,21 @@ cd `dirname $0`/..
 
 # create/update the virtual environment
 # --system-site-packages is present so we can use python-opencv with ease.
-virtualenv --system-site-packages ../virtualenv-sayit
-source ../virtualenv-sayit/bin/activate
+virtualenv_args="--system-site-packages"
+virtualenv_dir='../virtualenv-sayit'
+virtualenv_activate="$virtualenv_dir/bin/activate"
+if [ ! -f "$virtualenv_activate" ]
+then
+    virtualenv $virtualenv_args $virtualenv_dir
+fi
+source $virtualenv_activate
+
+# Upgrade pip to a secure version
+pip_version="$(pip --version)"
+if [ "$(echo -e 'pip 1.4\n'"$pip_version" | sort -V | head -1)" = "$pip_version" ]; then
+    curl -L -s https://bootstrap.pypa.io/get-pip.py | python
+fi
+
 pip install --requirement requirements.txt
 
 # make sure that there is no old code (the .py files may have been git deleted)
