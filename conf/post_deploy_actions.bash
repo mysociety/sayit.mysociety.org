8571a8169450c496e84d3c769789c4fbb948b26c
Matthew Somerville <matthew-github@dracos.co.uk>
2018-06-11 12:07:46 +0100

    Add scripts to rule them all.

--- conf/post_deploy_actions.bash
+++ conf/post_deploy_actions.bash
@@ -28,19 +28,4 @@ fi
 # virtualenv.
 pip install -U setuptools
 
-pip install --requirement requirements.txt
-
-# make sure that there is no old code (the .py files may have been git deleted)
-find . -name '*.pyc' -delete
-
-# get the database up to speed
-./manage.py syncdb
-./manage.py migrate
-
-# Install gems in order to compile the CSS
-bundle install --deployment --path ../gems --binstubs ../gem-bin
-
-# gather all the static files in one place
-./manage.py collectstatic --noinput
-
-./manage.py compilemessages
+script/update --deployment
